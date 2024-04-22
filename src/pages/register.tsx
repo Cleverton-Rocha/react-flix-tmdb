96ac828c14d85a589d82e938f40f18750dc4755c
Rocha <cl.rch09@icloud.com>
2024-04-22 15:31:10 -0300

    refactor: Refatora componente RegisterForm para adicionar formulário de registro em register-form.tsx

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,9 +1,11 @@
 import MoviePoster from '../components/movie-poster'
+import RegisterForm from '../components/register-form'
 
 const Register: React.FC = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
       <MoviePoster />
+      <RegisterForm />
     </div>
   )
 }
