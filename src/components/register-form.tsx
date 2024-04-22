f39477ff52ee0243dca63b3f4de9b95de86fa466
Rocha <cl.rch09@icloud.com>
2024-04-22 16:58:30 -0300

    refactor: Refatora componente RegisterForm para adicionar formulário de registro em register-form.tsx

--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -2,34 +2,48 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { z } from 'zod'
+import { useState } from 'react'
 
-const registerSchema = z.object({
-  email: z.string().email(),
-  username: z
-    .string()
-    .min(3, { message: 'Username must have at least 3 characters' })
-    .max(55, { message: 'Username must have at most 55 characters' })
-    .transform((username) => username.replace(/\s/g, ''))
-    .refine((username) => /^[a-zA-Z0-9]+$/.test(username), {
-      message: 'Username only accepts letters and numbers',
-    }),
-  password: z
-    .string()
-    .min(8, { message: 'Password must have at least 8 characters' })
-    .max(100, { message: 'Password must have at most 100 characters' })
-    .refine(
-      (password) =>
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password),
-      {
-        message:
-          'Password must have at least one uppercase letter, one lowercase letter and one number.',
-      },
-    ),
-})
+import PasswordToggle from './password-toggle'
+
+const registerSchema = z
+  .object({
+    email: z.string().email(),
+    username: z
+      .string()
+      .min(3, { message: 'Username must have at least 3 characters' })
+      .max(55, { message: 'Username must have at most 55 characters' })
+      .transform((username) => username.replace(/\s/g, ''))
+      .refine((username) => /^[a-zA-Z0-9]+$/.test(username), {
+        message: 'Username only accepts letters and numbers',
+      }),
+    password: z
+      .string()
+      .min(8, { message: 'Password must have at least 8 characters' })
+      .max(100, { message: 'Password must have at most 100 characters' })
+      .refine(
+        (password) =>
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(password),
+        {
+          message:
+            'Password must have at least one uppercase letter, one lowercase letter and one number.',
+        },
+      ),
+    confirmPassword: z
+      .string()
+      .min(8, { message: 'Password must have at least 8 characters' })
+      .max(100, { message: 'Password must have at most 100 characters' }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords must match.',
+    path: ['confirmPassword'],
+  })
 
 type RegisterFormValues = z.infer<typeof registerSchema>
 
 const RegisterForm: React.FC = () => {
+  const [inputType, setInputType] = useState('password')
+
   const {
     register,
     handleSubmit,
@@ -45,7 +59,7 @@ const RegisterForm: React.FC = () => {
   return (
     <>
       <div className="flex h-screen flex-col items-center justify-center bg-[#F9F9F9]">
-        <div className="flex h-[550px] w-[450px] flex-col gap-5 rounded-lg p-10">
+        <div className="flex h-[800px] w-[450px] flex-col gap-5 rounded-lg p-10">
           <div className="flex flex-col items-center gap-4 ">
             <h1 className="text-4xl font-semibold">React Flix!</h1>
             <span>
@@ -110,7 +124,7 @@ const RegisterForm: React.FC = () => {
                 </label>
                 <input
                   {...register('password')}
-                  type="password"
+                  type={inputType}
                   placeholder="Enter your password"
                   className="mb-2 mt-3 w-full rounded-full border bg-[#f1f1f1] px-4 py-2 text-lg outline-none placeholder:text-base"
                 />
@@ -122,7 +136,33 @@ const RegisterForm: React.FC = () => {
                 )}
               </div>
 
-              <div className="mt-6 flex justify-center">
+              <div className="mt-5">
+                <label htmlFor="password" className="text-sm">
+                  Confirm password
+                </label>
+                <div className="relative">
+                  <input
+                    {...register('confirmPassword')}
+                    type={inputType}
+                    placeholder="Confirm your password"
+                    className="mb-2 mt-3 w-full rounded-full border bg-[#f1f1f1] px-4 py-2 text-lg outline-none placeholder:text-base"
+                  />
+                </div>
+
+                {errors.confirmPassword && (
+                  <span className="flex justify-center px-4 text-sm text-red-500">
+                    {errors.confirmPassword.message}
+                  </span>
+                )}
+              </div>
+
+              <PasswordToggle
+                inputType={inputType}
+                setInputType={setInputType}
+                className="p-2"
+              />
+
+              <div className="mt-4 flex justify-center">
                 <button
                   type="submit"
                   className="w-2/3 rounded-full bg-black p-2 text-xl font-semibold transition duration-200 hover:scale-105"
