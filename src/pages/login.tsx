9699d3e0cdf000d522578eaf4028669c22295ca7
Rocha <cl.rch09@icloud.com>
2024-04-22 15:20:04 -0300

    refactor: corrige importação do componente LoginPoster para MoviePoster em login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,10 @@
 import LoginForm from '../components/login-form'
-import LoginPoster from '../components/login-poster'
+import MoviePoster from '../components/movie-poster'
 
 const Login: React.FC = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
-      <LoginPoster />
+      <MoviePoster />
       <LoginForm />
     </div>
   )
