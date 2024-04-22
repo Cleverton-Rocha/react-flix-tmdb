31086c7f20ade31aca0da969de77218a345558c0
Rocha <cl.rch09@icloud.com>
2024-04-22 15:20:11 -0300

    feat: adiciona página de registro com componente Register em register.tsx

---
+++ src/pages/register.tsx
@@ -0,0 +1,11 @@
+import MoviePoster from '../components/movie-poster'
+
+const Register: React.FC = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
+      <MoviePoster />
+    </div>
+  )
+}
+
+export default Register
