658a1ee84f086584a5d8cc87e573fc0e96473141
Rocha <cl.rch09@icloud.com>
2024-04-21 21:50:26 -0300

    feat: Adiciona roteamento para a página de login em main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
 import App from './App.tsx'
 import './index.css'
+import Login from './pages/login.tsx'
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Login />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
