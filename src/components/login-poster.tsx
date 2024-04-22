94166854137e53bed0129d251e6cd318cbeab7d5
Rocha <cl.rch09@icloud.com>
2024-04-21 21:50:39 -0300

    feat: Adiciona componente LoginPoster para a página de login

---
+++ src/components/login-poster.tsx
@@ -0,0 +1,15 @@
+const LoginPoster = () => {
+  return (
+    <>
+      <div>
+        <img
+          src="https://images.unsplash.com/photo-1616530940355-351fabd9524b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Movies"
+          className="hidden h-screen w-full object-cover object-center lg:block"
+        />
+      </div>
+    </>
+  )
+}
+
+export default LoginPoster
