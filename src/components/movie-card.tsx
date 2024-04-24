8a76db01ca3dea8da54b39c6983946fbbb90518a
Rocha <cl.rch09@icloud.com>
2024-04-24 13:37:20 -0300

    feat: Adiciona componente movie-card.tsx

--- src/components/movie-card.tsx
+++ src/components/movie-card.tsx
@@ -1,5 +1,28 @@
+import { useFetchTopRatedMovies } from '../queries/movie'
+
 const MovieCard: React.FC = () => {
-  return <div></div>
+  const topRated = useFetchTopRatedMovies()
+  const imageURL = import.meta.env.VITE_IMG
+
+  return (
+    <div className="p-12 text-white">
+      {topRated.isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="flex flex-wrap gap-12">
+          {topRated.data?.results.map((movie) => (
+            <div key={movie.id}>
+              <img
+                className="h-[220px] w-[150px] cursor-pointer rounded-xl transition duration-200 hover:scale-110"
+                src={imageURL + movie.poster_path}
+                alt=""
+              />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default MovieCard
