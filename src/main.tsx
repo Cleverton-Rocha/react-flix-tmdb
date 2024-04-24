7b9be5af10d02b9cfd2113c6a2064f25ac191ce4
Rocha <cl.rch09@icloud.com>
2024-04-24 00:38:07 -0300

    feat: Adiciona QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 import App from './App.tsx'
 import './index.css'
 import Login from './pages/login.tsx'
 import Register from './pages/register.tsx'
 import Home from './pages/home.tsx'
+import queryClient from './services/queryClient.ts'
 
 const router = createBrowserRouter([
   {
@@ -30,6 +32,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
