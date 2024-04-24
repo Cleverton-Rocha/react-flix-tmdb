42f9f97b8a0a2591dceeef9d25cbb6e2beb5f983
Rocha <cl.rch09@icloud.com>
2024-04-24 00:40:47 -0300

    refactor: Corrige tipagem em types.ts

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,16 @@
-export type loginData = {
+export type LoginData = {
+  username: string
+  password: string
+}
+
+export type LoginResponse = {
+  accessToken: string
+  username: string
+  expiresIn: string
+}
+
+export type RegisterData = {
+  email: string
   username: string
   password: string
 }
