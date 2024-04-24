2157a719c4debc4968930200241f19de0d174d74
Rocha <cl.rch09@icloud.com>
2024-04-24 13:36:30 -0300

    feat: Adiciona função getTopRatedMovies

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,34 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
 
-import { LoginData, RegisterData } from '../utils/types'
+import {
+  LoginData,
+  LoginResponse,
+  MoviePage,
+  RegisterData,
+  RegisterResponse,
+} from '../utils/types'
 
 const api = axios.create({
   baseURL: 'http://localhost:8080',
 })
 
+const token = Cookies.get('token')
+
 export async function register(registerData: RegisterData) {
-  const { data } = await api.post('/api/user', registerData)
+  const { data } = await api.post<RegisterResponse>('/api/user', registerData)
   return data
 }
 
 export async function login(loginData: LoginData) {
-  const { data } = await api.post('/login', loginData)
+  const { data } = await api.post<LoginResponse>('/login', loginData)
+  return data
+}
+
+export async function getTopRatedMovies(pageNumber: number = 1) {
+  const { data } = await api.get<MoviePage>(
+    `/api/movies/top-rated?page=${pageNumber}`,
+    { headers: { Authorization: `Bearer ${token}` } },
+  )
   return data
 }
