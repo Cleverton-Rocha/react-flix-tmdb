09c4f4eff403513271073e51ce541ab6ab2e0c55
Rocha <cl.rch09@icloud.com>
2024-04-24 00:39:57 -0300

    feat: Adiciona função de registro de usuário e atualiza função de login em api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,17 @@
 import axios from 'axios'
 
-import { loginData } from '../utils/types'
+import { LoginData, RegisterData } from '../utils/types'
 
 const api = axios.create({
   baseURL: 'http://localhost:8080',
 })
 
-export async function login(loginData: loginData) {
+export async function register(registerData: RegisterData) {
+  const { data } = await api.post('/api/user', registerData)
+  return data
+}
+
+export async function login(loginData: LoginData) {
   const { data } = await api.post('/login', loginData)
   return data
 }
