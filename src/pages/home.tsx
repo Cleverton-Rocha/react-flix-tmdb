c60ddc326e50e90b7585a26dd0d4564b787455e1
Rocha <cl.rch09@icloud.com>
2024-04-25 00:04:55 -0300

    style: Corrige estilo do componente Home na página home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,7 @@ import MovieCard from '../components/movie-card'
 
 const Home: React.FC = () => {
   return (
-    <div className="h-screen bg-[#131127]">
+    <div className="h-full bg-[#131127]">
       <Header />
       <MovieCard />
     </div>
