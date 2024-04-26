a9ab31353430c34c92e1d7c0c2d4c9e0d677d8f5
Rocha <cl.rch09@icloud.com>
2024-04-25 23:48:44 -0300

    feat: Adiciona componente HomeBanner

---
+++ src/components/home-banner.tsx
@@ -0,0 +1,17 @@
+const HomeBanner = () => {
+  return (
+    <div className="grid grid-cols-2 px-80">
+      <span className="my-auto w-[700px] p-12 text-6xl text-white">
+        Take your movie experience to the next{' '}
+        <span className="text-red-700">level</span>
+      </span>
+      <img
+        className="h-[600px] object-cover"
+        src="https://image.tmdb.org/t/p/original/qi6Edc1OPcyENecGtz8TF0DUr9e.jpg"
+        alt="Movie Banner"
+      />
+    </div>
+  )
+}
+
+export default HomeBanner
