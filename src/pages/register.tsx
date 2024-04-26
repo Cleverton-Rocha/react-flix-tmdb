f7b2e8f9ec105bcef82205484bb2cf22a038a462
Rocha <cl.rch09@icloud.com>
2024-04-25 23:49:55 -0300

    refactor: Refatora importação do componente MoviePoster para LoginBanner

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,10 +1,10 @@
-import MoviePoster from '../components/movie-poster'
+import LoginBanner from '../components/login-banner'
 import RegisterForm from '../components/register-form'
 
 const Register: React.FC = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2">
-      <MoviePoster />
+      <LoginBanner />
       <RegisterForm />
     </div>
   )
