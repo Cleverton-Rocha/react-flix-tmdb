4f3c90df5de574e29d62e8d9bb0b8eea5bc0e9d7
Rocha <cl.rch09@icloud.com>
2024-04-27 01:11:26 -0300

    feat: Adiciona arquivo genres.ts com lista de gêneros de filmes

---
+++ src/utils/genres.ts
@@ -0,0 +1,67 @@
+export const genres = [
+  {
+    name: 'ação',
+    id: 28,
+    image: 'https://image.tmdb.org/t/p/w500/A4kvp7vY1BDLrrQIagRCffLKj1t.jpg',
+  },
+  {
+    name: 'aventura',
+    id: 12,
+    image: 'https://image.tmdb.org/t/p/w500/8LJJjLjAzAwXS40S5mx79PJ2jSs.jpg ',
+  },
+  {
+    name: 'animação',
+    id: 16,
+    image: 'https://image.tmdb.org/t/p/w500/aNK6MA5EApIo0UJE7ZWSYcZBJKy.jpg',
+  },
+  {
+    name: 'comédia',
+    id: 35,
+    image: 'https://image.tmdb.org/t/p/w500/7xvLGlzqlMdJi7b2w1SKycsbpVI.jpg',
+  },
+  {
+    name: 'crime',
+    id: 80,
+    image: 'https://image.tmdb.org/t/p/w500/iQ5ztdjvteGeboxtmRdXEChJOHh.jpg',
+  },
+  {
+    name: 'documentário',
+    id: 99,
+    image: 'https://image.tmdb.org/t/p/w500/j0DbcqFzXB6Bk9fqvgcp1iZG8wj.jpg',
+  },
+  {
+    name: 'drama',
+    id: 18,
+    image: 'https://image.tmdb.org/t/p/w500/1OsQJEoSXBjduuCvDOlRhoEUaHu.jpg',
+  },
+  {
+    name: 'fantasia',
+    id: 14,
+    image: 'https://image.tmdb.org/t/p/w500/ub4EHdVBVm3nHJiamU0gGcKS4ae.jpg',
+  },
+  {
+    name: 'terror',
+    id: 27,
+    image: 'https://image.tmdb.org/t/p/w500/5E4nVa3dXTHG2zx9iO7b0UIMsa7.jpg',
+  },
+  {
+    name: 'mistério',
+    id: 9648,
+    image: 'https://image.tmdb.org/t/p/w500/fhQoQfejY1hUcwyuLgpBrYs6uFt.jpg',
+  },
+  {
+    name: 'romance',
+    id: 10749,
+    image: 'https://image.tmdb.org/t/p/w500/tPx1HEiS5zd9mLvtb52YY66dkEO.jpg',
+  },
+  {
+    name: 'ficção científica',
+    id: 878,
+    image: 'https://image.tmdb.org/t/p/w500/nCbkOyOMTEwlEV0LtCOvCnwEONA.jpg',
+  },
+  {
+    name: 'guerra',
+    id: 10752,
+    image: 'https://image.tmdb.org/t/p/w500/hMLxNLCXRDd62acfCBn6mIyW1HU.jpg',
+  },
+]
