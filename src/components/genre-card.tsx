581c64e5fd04a6a6c8b5004867b2ab0ab8e82f50
Rocha <cl.rch09@icloud.com>
2024-04-29 23:10:30 -0300

    refactor: Refatora componente GenreCard

--- src/components/genre-card.tsx
+++ src/components/genre-card.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 
+import { cn } from '../utils/utils'
+
 type GenreCardProps = {
   genreName: string
+  className?: string
+  onClick: () => void
 }
 
-const GenreCard: React.FC<GenreCardProps> = ({ genreName }) => {
+const GenreCard: React.FC<GenreCardProps> = ({
+  genreName,
+  onClick,
+  className,
+}) => {
   return (
     <>
-      <div className="mx-auto flex items-center">
-        <span className="absolute w-[180px] rounded-full px-4 py-3 text-center text-xl text-white hover:bg-[#131313]">
+      <div
+        onClick={onClick}
+        className="mx-auto flex cursor-pointer items-center"
+      >
+        <span
+          className={cn(
+            'absolute w-[180px] rounded-full px-4 py-3 text-center text-xl text-white hover:bg-[#131313]',
+            className,
+          )}
+        >
           {genreName}
         </span>
       </div>
