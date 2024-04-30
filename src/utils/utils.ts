4208f57b53fc673960b4568bb14c7107e9332dba
Rocha <cl.rch09@icloud.com>
2024-04-29 23:13:53 -0300

    refactor: Refatora arquivo utils.ts para adicionar configurações responsivas de carrossel

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 1024 },
+    items: 9,
+    slidesToSlide: 3,
+  },
+  desktop: {
+    breakpoint: { max: 1024, min: 800 },
+    items: 5,
+    slidesToSlide: 3,
+  },
+  tablet: {
+    breakpoint: { max: 800, min: 464 },
+    items: 2,
+    slidesToSlide: 3,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 3,
+  },
+}
