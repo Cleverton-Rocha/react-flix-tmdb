4adfbd75d62f96aab189a6de55f39de29b3518b9
Rocha <cl.rch09@icloud.com>
2024-04-29 23:12:45 -0300

    refactor: Refatora componente Home para adicionar carrossel de gêneros e filmes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,24 @@
+import GenreCarousel from '../components/genre-carousel'
 import Header from '../components/header'
-import MovieCard from '../components/movie-card'
+import HomeBanner from '../components/home-banner'
+import MovieCarousel from '../components/movie-carousel'
+import { useFetchTopRatedMovies } from '../queries/movie'
 
 const Home: React.FC = () => {
+  const { data } = useFetchTopRatedMovies()
+
   return (
-    <div className="h-full bg-[#131127]">
+    <div className="h-full bg-black px-12">
       <Header />
-      <MovieCard />
+      <HomeBanner />
+      <div className="flex flex-col gap-4">
+        <MovieCarousel
+          label="Top rated movies"
+          redirectTo="/top-rated"
+          results={data?.results ?? []}
+        />
+        <GenreCarousel />
+      </div>
     </div>
   )
 }
