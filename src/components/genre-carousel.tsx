6e564accb21f6c8130353e830af0e2066b20f5f7
Rocha <cl.rch09@icloud.com>
2024-04-29 23:10:45 -0300

    feat: Adiciona funcionalidade de scroll infinito

--- src/components/genre-carousel.tsx
+++ src/components/genre-carousel.tsx
@@ -1,49 +1,96 @@
 import Carousel from 'react-multi-carousel'
+import { useEffect, useState } from 'react'
+import React from 'react'
+import { useInView } from 'react-intersection-observer'
+import { ChevronUp } from 'lucide-react'
 
 import { genres } from '../utils/genres'
+import { cn, responsive } from '../utils/utils'
+import { useFetchMovieByGenreInfinityQuery } from '../queries/movie'
+import { Movie } from '../utils/types'
 
 import GenreCard from './genre-card'
+import MovieCard from './movie-card'
+import LoadingSpinner from './loading-spinner'
 
-type GenreCarouselProps = {
-  label: string
-}
+const GenreCarousel: React.FC = () => {
+  const [genreName, setGenreName] = useState('ação')
+  const [showScroll, setShowScroll] = useState(false)
 
-const GenreCarousel: React.FC<GenreCarouselProps> = ({ label }) => {
-  const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 1024 },
-      items: 9,
-      slidesToSlide: 3,
-    },
-    desktop: {
-      breakpoint: { max: 1024, min: 800 },
-      items: 5,
-      slidesToSlide: 3,
-    },
-    tablet: {
-      breakpoint: { max: 800, min: 464 },
-      items: 2,
-      slidesToSlide: 3,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 3,
-    },
+  const { data, status, fetchNextPage, isFetchingNextPage } =
+    useFetchMovieByGenreInfinityQuery(genreName)
+
+  const { ref, inView } = useInView()
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage()
+    }
+  }, [fetchNextPage, inView])
+
+  useEffect(() => {
+    const checkScrollTop = () => {
+      const shouldShowScroll = window.scrollY > 600
+      setShowScroll(shouldShowScroll)
+    }
+
+    window.addEventListener('scroll', checkScrollTop)
+
+    return () => {
+      window.removeEventListener('scroll', checkScrollTop)
+    }
+  }, [])
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
   }
+
   return (
-    <div className="">
-      <div className="flex items-center justify-between">
-        <h1 className="select-none text-2xl font-semibold text-white">
-          {label}
-        </h1>
+    <>
+      <div className="">
+        <div className="flex items-center justify-between">
+          <h1 className="select-none text-2xl font-semibold text-white">
+            Genres
+          </h1>
+        </div>
+        <Carousel className="h-[100px] w-full" responsive={responsive}>
+          {genres.map((genre) => (
+            <GenreCard
+              className={cn(
+                genreName === genre.name.toLowerCase() &&
+                  'border-2 border-red-700 hover:border-none',
+              )}
+              onClick={() => setGenreName(genre.name.toLowerCase())}
+              genreName={genre.name}
+              key={genre.id}
+            />
+          ))}
+        </Carousel>
+        <div>
+          {status === 'pending' && <LoadingSpinner />}
+          <div className="grid grid-cols-9 gap-12 pr-12">
+            {data?.pages.map((page, i) => (
+              <React.Fragment key={i}>
+                {page.results.map((movie: Movie) => (
+                  <MovieCard
+                    movieId={movie.id}
+                    key={movie.id}
+                    moviePoster={movie.poster_path}
+                    movieTitle={movie.title}
+                  />
+                ))}
+              </React.Fragment>
+            ))}
+            {isFetchingNextPage && <LoadingSpinner />}
+            <div ref={ref}></div>
+          </div>
+        </div>
       </div>
-      <Carousel className="h-[100px] w-full" responsive={responsive}>
-        {genres.map((genre) => (
-          <GenreCard genreName={genre.name} />
-        ))}
-      </Carousel>
-    </div>
+      <ChevronUp
+        className={`fixed bottom-5 right-5 h-10 w-10 cursor-pointer rounded-full border-2 text-white transition duration-200 hover:text-red-700 ${showScroll ? 'visible' : 'invisible'}`}
+        onClick={scrollTop}
+      />
+    </>
   )
 }
 
